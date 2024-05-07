5d379ad89b288c6cb380c1ba161c0044bb76e131
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2024-05-06 19:49:59 -0600

    Se modifica el parámetro LANGUAGE_CODE en el archivo settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -104,7 +104,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-MX'
 
 TIME_ZONE = 'UTC'
 
